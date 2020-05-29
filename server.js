3d188ef3793123fcf5ce3ca26e5f244ed21d1ce2
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-05-29 07:42:51 -0700

    added server functions

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
+const express = require("express");
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://user22:<password>@heroku2275-jqrd8.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const mongoose = require("mongoose");
 
-////https://www.youtube.com/watch?v=WDrU305J1yw
+const routes = require("./routes");
 
-// https://www.youtube.com/watch?v=qXIG8iKO7Fo&feature=youtu.be&list=PLOFmg4xbN_TPrB6w4rThsFanVxJI_SfER
+const app = express();
 
-// user22: Hav123
+const PORT = process.env.PORT || 3001;
+
+// Configure body parsing for AJAX requests
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+//Serve up static assets
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://user22:<password>@heroku2275-jqrd8.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+// ////https://www.youtube.com/watch?v=WDrU305J1yw
+
+// // https://www.youtube.com/watch?v=qXIG8iKO7Fo&feature=youtu.be&list=PLOFmg4xbN_TPrB6w4rThsFanVxJI_SfER
+
+// // user22: Hav123
